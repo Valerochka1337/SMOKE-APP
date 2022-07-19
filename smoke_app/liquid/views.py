125b343344abd040ec1d5e4baee64f2606c7b74e
Valerochka1337 <valerochkayartsev88@gmail.com>
2022-07-19 16:45:07 +0500

    permissions_added

--- smoke_app/liquid/views.py
+++ smoke_app/liquid/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Liquid
 from .serializers import LiquidSerializer
+from .permissions import IsAdminOrReadOnly
 
 class LiquidViewSet(viewsets.ModelViewSet):
     queryset = Liquid.objects.all()
     serializer_class = LiquidSerializer
+    permission_classes = (IsAdminOrReadOnly,)
